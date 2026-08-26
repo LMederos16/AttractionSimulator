61f63f61a49bf8535e9204f13c324d05346410c3
LMederos1 <lmederos1@hotmail.com>
2026-08-26 19:01:27 -0400

    Adds basic attraction status information

--- AttractionSimulator/Program.cs
+++ AttractionSimulator/Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.WriteLine("Attraction Simulator Starting...");
+
+string rideName = "CodeCoaster";
+int vehicleCapacity = 16;
+bool isOperating = true;
+
+Console.WriteLine($"Attraction Name: {rideName}");
+Console.WriteLine($"Vehicle Capacity: {vehicleCapacity}");
+Console.WriteLine($"Currently Operating: {isOperating}");
