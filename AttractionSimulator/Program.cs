3c7269da90ec738395f374c948890b700c5d1b2f
LMederos1 <lmederos1@hotmail.com>
2026-08-26 19:19:56 -0400

    Add status command handling

--- AttractionSimulator/Program.cs
+++ AttractionSimulator/Program.cs
@@ -4,6 +4,18 @@ string rideName = "CodeCoaster";
 int vehicleCapacity = 16;
 bool isOperating = true;
 
-Console.WriteLine($"Attraction Name: {rideName}");
-Console.WriteLine($"Vehicle Capacity: {vehicleCapacity}");
-Console.WriteLine($"Currently Operating: {isOperating}");
+// Status right now only notices "status"
+Console.Write("Enter a command:");
+string? command = Console.ReadLine();
+
+if(command == "status")
+{
+    Console.WriteLine("Showing attraction status");
+    Console.WriteLine($"Attraction Name: {rideName}");
+    Console.WriteLine($"Vehicle Capacity: {vehicleCapacity}");
+    Console.WriteLine($"Currently Operating: {isOperating}");
+}
+else
+{
+    Console.WriteLine("Unknown command");
+}
