126bba6ac97c19c76b1f78806a0f570ae8690c23
LMederos1 <lmederos1@hotmail.com>
2026-08-27 12:29:13 -0400

    Fixed typo and cleanup code

--- AttractionSimulator/RideVehicle.cs
+++ AttractionSimulator/RideVehicle.cs
@@ -4,27 +4,27 @@
     {
         public int VehichleId { get; set; }
         public int Capacity { get; set; }
-        public int PassangerCount { get; set; }
+        public int PassengerCount { get; set; }
         public bool RestraintsEngaged { get; set; }
 
         public RideVehicle(int vehicleId, int capacity, int passangerCount)
         {
             VehichleId = vehicleId;
             Capacity = capacity;
-            PassangerCount = passangerCount;
+            PassengerCount = passangerCount;
         }
 
         public void LoadPassangers(int number)
         {
-            PassangerCount = PassangerCount + number;
-            if (PassangerCount > Capacity)
+            if (PassengerCount + number > Capacity)
             {
-                Console.WriteLine("This part is too big to load. Please wait for the next train.");
-                PassangerCount = PassangerCount - number;
+                Console.WriteLine("This party is too big to load. Please wait for the next train.");
             }
             else
             {
-                Console.WriteLine($"Loaded! There are now {PassangerCount} on this train!");
+                PassengerCount = PassengerCount + number;
+
+                Console.WriteLine($"Loaded! There are now {PassengerCount} on this train!");
             }
         }
     }
