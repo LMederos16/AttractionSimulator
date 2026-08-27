a5812ef1e9fb452027caf0b53955ff93b5fba7fb
LMederos1 <lmederos1@hotmail.com>
2026-08-26 23:09:08 -0400

    Created different commands to change the status of the ride. Added start, stop, and exit. Will loop until exit.

--- AttractionSimulator/Program.cs
+++ AttractionSimulator/Program.cs
@@ -4,18 +4,38 @@ string rideName = "CodeCoaster";
 int vehicleCapacity = 16;
 bool isOperating = true;
 
-// Status right now only notices "status"
-Console.Write("Enter a command:");
-string? command = Console.ReadLine();
+while (true)
+{
+    // Command right now only notices "status", "start", "stop", and "exit" all lowercase for now
+    Console.Write("Enter a command (status, start, stop, or exit): ");
+    string? command = Console.ReadLine();
 
-if(command == "status")
-{
-    Console.WriteLine("Showing attraction status");
-    Console.WriteLine($"Attraction Name: {rideName}");
-    Console.WriteLine($"Vehicle Capacity: {vehicleCapacity}");
-    Console.WriteLine($"Currently Operating: {isOperating}");
-}
-else
-{
-    Console.WriteLine("Unknown command");
+    if (command == "status")
+    {
+        Console.WriteLine("Showing attraction status");
+        Console.WriteLine($"Attraction Name: {rideName}");
+        Console.WriteLine($"Vehicle Capacity: {vehicleCapacity}");
+        Console.WriteLine($"Currently Operating: {isOperating}");
+    }
+    else if (command == "start")
+    {
+        Console.WriteLine("Attraction Started");
+        isOperating = true;
+    }
+    else if (command == "stop")
+    {
+        Console.WriteLine("Attraction Stopped");
+        isOperating = false;
+    }
+    else if (command == "exit")
+    {
+        Console.WriteLine("Exiting...");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Unknown command");
+    }
+    // Creates spacing for different outputs
+    Console.WriteLine();
 }
