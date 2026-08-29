71ab93ca422ebb48819a022867036c85245f5f7d
LMederos1 <lmederos1@hotmail.com>
2026-08-29 14:18:56 -0400

    Added property List<RideVehicle> and added 3 Vehicles to the list

--- AttractionSimulator/Attraction.cs
+++ AttractionSimulator/Attraction.cs
@@ -3,20 +3,26 @@
     internal class Attraction
     {
         public string Name { get; }
-        public int VehicleCapacity { get; }
+        public int MaxVehicles { get; } = 6;
         public bool IsOperating { get; private set; }
+        private List<RideVehicle> Vehicles { get; }
 
-        public Attraction(string name, int vehicleCapacity)
+        public Attraction(string name)
         {
             Name = name;
-            VehicleCapacity = vehicleCapacity;
             IsOperating = false;
+            Vehicles = new List<RideVehicle>();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Vehicles.Add(new RideVehicle(i));
+            }
         }
         public void DisplayStatus()
         {
             Console.WriteLine("Showing attraction status");
             Console.WriteLine($"Attraction Name: {Name}");
-            Console.WriteLine($"Vehicle Capacity: {VehicleCapacity}");
+            Console.WriteLine($"Vehicle Capacity: {MaxVehicles}");
             Console.WriteLine($"Currently Operating: {IsOperating}");
         }
         public void Start()
